414b8ec474c4a28b8796b794173a9fc72d6aad86
ratasorin <ratasorin0@gmail.com>
2022-06-15 16:09:14 +0300

    [Feature] Stack the custom scrollbar on top of the existing scrollbar

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -3,12 +3,13 @@ const CARDS = 50;
 const Section = () => {
   return (
     <div className="absolute left-0 top-0 w-screen h-screen bg-slate-50 p-6 flex items-center">
-      <div className="relative w-full overflow-x-scroll overflow">
+      <div className="relative w-full overflow-x-scroll overflow scroll-overlap">
         <div className="min-w-fit h-auto flex flex-row">
           {Array.from({ length: CARDS }).map((_, i) => (
             <div className="w-72 h-96 bg-slate-400 m-6"></div>
           ))}
         </div>
+        <div className="absolute h-2 w-full bottom-0 bg-slate-400"></div>
       </div>
     </div>
   );
