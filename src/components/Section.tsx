9aa5440c0fb1b0d3623b046a34cf95bb54e8e925
ratasorin <ratasorin0@gmail.com>
2022-06-15 23:08:04 +0300

    [Feature] Only allow thumb to move on track

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Draggable from "react-draggable";
 
 const CARDS = 50;
 
 const Section = () => {
+  const [bounds, setBounds] = useState<{ left: number; right: number } | null>(
+    null
+  );
+  const measureTrack = useCallback((track: HTMLDivElement) => {
+    if (!track) return null;
+    const thumb = track.firstChild as HTMLElement;
+    setBounds({
+      left: track.clientLeft,
+      right: track.clientWidth - thumb.clientWidth,
+    });
+  }, []);
+
   return (
     <div className="absolute left-0 top-0 w-screen h-screen bg-slate-50 p-6">
       <div className="relative w-full overflow-x-scroll min-h-fit bg-yellow-300">
@@ -15,9 +27,9 @@ const Section = () => {
           </div>
         </div>
       </div>
-      <div className="relative h-1 w-full bg-red-400">
-        <Draggable axis="x">
-          <div className="absolute w-10 h-5 bottom-0 bg-red-200 left-0"></div>
+      <div ref={measureTrack} className="relative h-1 w-full bg-red-400 my-5">
+        <Draggable bounds={bounds ? bounds : { left: 0, right: 0 }} axis="x">
+          <div className="absolute w-10 h-5 bg-red-200 -top-2"></div>
         </Draggable>
       </div>
     </div>
