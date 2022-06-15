1fefe4da14da54a43672d232dcfc88a076ed2bb0
ratasorin <ratasorin0@gmail.com>
2022-06-15 22:55:31 +0300

    [Feature] Use react-draggable for interacting with the custom scrollbar

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,19 +1,24 @@
+import { useState } from "react";
+import Draggable from "react-draggable";
+
 const CARDS = 50;
 
 const Section = () => {
   return (
-    <div className="absolute left-0 top-0 w-screen h-screen bg-slate-50 p-6 flex items-center">
-      <div className="relative w-full overflow-x-scroll overflow-hidden scroll-overlap">
+    <div className="absolute left-0 top-0 w-screen h-screen bg-slate-50 p-6">
+      <div className="relative w-full overflow-x-scroll min-h-fit bg-yellow-300">
         <div className="relative min-w-fit">
           <div className="min-w-fit h-auto flex flex-row">
             {Array.from({ length: CARDS }).map((_, i) => (
               <div className="w-72 h-96 bg-slate-400 m-6"></div>
             ))}
           </div>
-          <div className="absolute z-50 h-1 min-w-full -translate-y-[12px] bg-slate-400">
-            <div className="absolute w-10 h-5 bg-slate-200 -translate-y-1/2 left-0"></div>
-          </div>
         </div>
+      </div>
+      <div className="relative h-1 w-full bg-red-400">
+        <Draggable axis="x">
+          <div className="absolute w-10 h-5 bottom-0 bg-red-200 left-0"></div>
+        </Draggable>
       </div>
     </div>
   );
