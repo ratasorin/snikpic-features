8df9d483ebecd212bd7186a1c63cac7f0dbf4134
ratasorin <ratasorin0@gmail.com>
2022-06-15 16:25:48 +0300

    [Feature] Add button to the custom scrollbar

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -3,14 +3,16 @@ const CARDS = 50;
 const Section = () => {
   return (
     <div className="absolute left-0 top-0 w-screen h-screen bg-slate-50 p-6 flex items-center">
-      <div className="relative w-full overflow-x-scroll overflow scroll-overlap">
+      <div className="relative w-full overflow-x-scroll overflow-hidden scroll-overlap">
         <div className="relative min-w-fit">
           <div className="min-w-fit h-auto flex flex-row">
             {Array.from({ length: CARDS }).map((_, i) => (
               <div className="w-72 h-96 bg-slate-400 m-6"></div>
             ))}
           </div>
-          <div className="absolute h-2 min-w-full bottom-0 bg-slate-400"></div>
+          <div className="absolute z-50 h-1 min-w-full -translate-y-[12px] bg-slate-400">
+            <div className="absolute w-10 h-5 bg-slate-200 -translate-y-1/2 left-0"></div>
+          </div>
         </div>
       </div>
     </div>
