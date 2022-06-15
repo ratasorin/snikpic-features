36644ed6f94bf14ee4672db8b215f4841fa4e928
ratasorin <ratasorin0@gmail.com>
2022-06-16 00:10:48 +0300

    [Feature] Create desired drag to scroll mechanism

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,7 +1,12 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import Draggable from "react-draggable";
 
 const CARDS = 50;
+
+const convertThumbMovementToWindowMovement = (
+  unit: number,
+  thumbPosition: number
+) => unit * thumbPosition;
 
 const Section = () => {
   const [bounds, setBounds] = useState<{ left: number; right: number } | null>(
@@ -21,12 +26,35 @@ const Section = () => {
     [thumb]
   );
 
-  // const [window, setWindow] = useEffect({});
+  const [slideWindow, setSlideWindow] = useState<HTMLDivElement | null>(null);
+  const [cardsContainer, setCardsContainer] = useState<HTMLDivElement | null>(
+    null
+  );
 
+  const unit = useMemo(() => {
+    console.log({ slideWindow, cardsContainer, bounds });
+    if (!slideWindow || !cardsContainer || !bounds) return 0;
+    const trackWidth = bounds.right - bounds.left;
+    const unit =
+      (cardsContainer.clientWidth - slideWindow.clientWidth) / trackWidth;
+    console.log({ unit });
+    return unit;
+  }, [slideWindow, cardsContainer, bounds]);
+
+  const [thumbLeft, setThumbLeft] = useState(0);
   return (
     <div className="absolute left-0 top-0 w-screen h-screen bg-slate-50 p-6">
-      <div className="relative w-full overflow-x-scroll min-h-fit bg-yellow-300">
-        <div className="relative min-w-fit">
+      <div
+        ref={setSlideWindow}
+        className="relative w-full overflow-x-scroll min-h-fit bg-yellow-300"
+      >
+        <div
+          style={{
+            left: -convertThumbMovementToWindowMovement(unit, thumbLeft),
+          }}
+          ref={setCardsContainer}
+          className="relative min-w-fit"
+        >
           <div className="min-w-fit h-auto flex flex-row">
             {Array.from({ length: CARDS }).map((_, i) => (
               <div className="w-72 h-96 bg-slate-400 m-6"></div>
@@ -38,6 +66,7 @@ const Section = () => {
         <Draggable
           onDrag={(_, data) => {
             console.log("DRAGGING", data);
+            setThumbLeft(data.x + thumb!.clientWidth / 2);
           }}
           bounds={bounds ? bounds : { left: 0, right: 0 }}
           axis="x"
