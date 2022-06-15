6a436e70f1e68fa3cbf25ed89a08c9ded3a312d7
ratasorin <ratasorin0@gmail.com>
2022-06-15 15:58:28 +0300

    [Feature] Create demo that requires horizontal scroll

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Section from "../components/Section";
+
+const Home: NextPage = () => {
+  return <Section />;
+};
+
+export default Home;
